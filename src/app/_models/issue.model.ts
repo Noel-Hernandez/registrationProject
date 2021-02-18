c23e58c8dacc38f06131d357b90b450c91903652
Noel-Hernandez <noel.hernandez@ucr.ac.cr>
2021-02-17 22:06:31 -0600

    Initial commit

---
+++ src/app/_models/issue.model.ts
@@ -0,0 +1,11 @@
+export class Issue {
+    Id: number;
+    IdClient: number;
+    Description: string;
+    TimeStamp: Date;
+    ContactPhone: string;
+    Address: string;
+    ContactEmail:string;
+    Status: string;
+    SupportAsiigned:string;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
+import { HomeComponent } from './home';;
+import { IssueListComponent } from './issue-list/issue-list.component'
+;
+import { IssueDetailComponent } from './issue-detail/issue-detail.component'
+;
+import { AddIssueComponent } from './add-issue/add-issue.component';
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent
+,
+        IssueListComponent ,
+        IssueDetailComponent ,
+        AddIssueComponent  ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
